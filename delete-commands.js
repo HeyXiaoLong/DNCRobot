aee47dfb592adf3faf42b336673a2b936dd46969
林晓龙 <112756121+HeyXiaoLong@users.noreply.github.com>
2022-11-22 23:06:44 +0700

    Initial commit

---
+++ delete-commands.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('node:fs');
+
+dotenv.config();
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+// for guild-based commands
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// for global commands
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
